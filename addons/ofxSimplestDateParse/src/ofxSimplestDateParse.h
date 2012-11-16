ccdc010f9fb8b80829776de0b1cac8637c5176c6
hiroyuki hori - horristic <hiroyuki@rhizomatiks.com>
2012-11-17 01:52:22 +0900

    from genba

---
+++ addons/ofxSimplestDateParse/src/ofxSimplestDateParse.h
@@ -0,0 +1,14 @@
+//
+//  ofxSimplestDateParse.h
+//  motherfarmLED
+//
+//  Created by 堀 宏行 on 11/16/12.
+//
+//
+
+#ifndef motherfarmLED_ofxSimplestDateParse_h
+#define motherfarmLED_ofxSimplestDateParse_h
+
+
+
+#endif
---
+++ src/StatusManagement.h
@@ -0,0 +1,14 @@
+//
+//  StatusManagement.h
+//  motherfarmLED
+//
+//  Created by 堀 宏行 on 11/16/12.
+//
+//
+
+#ifndef motherfarmLED_StatusManagement_h
+#define motherfarmLED_StatusManagement_h
+
+
+
+#endif
